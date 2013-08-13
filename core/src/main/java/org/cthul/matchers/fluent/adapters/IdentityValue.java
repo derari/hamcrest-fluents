9d66ffce9c6e5d21d4a6279ba56fb7ed3fa79f37
at <arian.treffer@hpi.uni-potsdam.de>
2013-08-13 08:37:04 +0200

    Removed IdentityValue#name

--- core/src/main/java/org/cthul/matchers/fluent/adapters/IdentityValue.java
+++ core/src/main/java/org/cthul/matchers/fluent/adapters/IdentityValue.java
@@ -2,7 +2,6 @@ package org.cthul.matchers.fluent.adapters;
 
 import org.cthul.matchers.fluent.value.MatchValue;
 import org.hamcrest.Description;
-import org.hamcrest.SelfDescribing;
 
 /**
  *
@@ -19,59 +18,24 @@ public class IdentityValue<Value> extends ConvertingAdapter<Value, Value> {
         return INSTANCE.adapt(value);
     }
     
-    private final String name;
-
     public IdentityValue() {
         super(Object.class);
-        this.name = null;
-    }
-
-    public IdentityValue(String name) {
-        super(Object.class);
-        this.name = name;
     }
 
     @Override
     public MatchValue<Value> adapt(MatchValue<Value> value) {
-        if (name == null) {
-            // shortcut, this adapter adds nothing
-            return value;
-        } else {
-            return super.adapt(value);
-        }
+        // shortcut, this adapter adds nothing
+        return value;
     }
 
     @Override
     protected boolean hasDescription() {
-        return name != null;
+        return false;
     }
 
     @Override
     public void describeTo(Description description) {
-        if (name == null) {
-            description.appendText("identity");
-        } else {
-            description.appendText(name);
-        }
-    }
-
-    @Override
-    public void describeProducer(SelfDescribing producer, Description description) {
-        if (name == null) {
-            super.describeProducer(producer, description);
-        } else {
-            description.appendText(name);
-        }
-    }
-
-    @Override
-    public void describeConsumer(SelfDescribing consumer, Description description) {
-//        if (name == null) {
-            super.describeConsumer(consumer, description);
-//        } else {
-//            // FIXME
-//            description.appendText(name);
-//        }
+        description.appendText("identity");
     }
 
     @Override
